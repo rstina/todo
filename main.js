6373e315d254f41f4e6823108a222ccceef999d9
rstina <53047495+rstina@users.noreply.github.com>
2019-11-05 14:48:37 +0100

    all G specifics working

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 const submitTodoBtn = document.querySelector('#addTodoBtn')
 const todoListArray = []
+const todoField = document.querySelector('#todoText')
+const filterField = document.querySelector('#filter')
+const ulList = document.querySelector('#todoList')
 
 submitTodoBtn.addEventListener('click',function(event){
     event.preventDefault()
-    todoListArray.push(document.querySelector('#todoText').value)
-    document.querySelector('#todoText').value = ''
-    console.log(todoListArray)
+    todoListArray.push(todoField.value)
+    todoField.value = ''
+    filterField.value = ''
     displayList(todoListArray)
 })
 
-// vilken array tittar jag på??
-// varför inte delete efter att jag filtrerat
-
-const filterField = document.querySelector('#filter')
-const ulList = document.querySelector('#todoList')
-// display list
 function displayList(arr){
     ulList.innerHTML = ''
     arr.forEach( function(todo) {
@@ -25,16 +22,13 @@ function displayList(arr){
         div.appendChild(li)
         const deleteBtn = document.createElement('button')
         deleteBtn.addEventListener('click',function(event){
-            console.log(todoListArray)
             todoListArray.splice(todoListArray.indexOf(todo),1)
             event.currentTarget.closest('div').remove()
-            console.log(todoListArray)
         })
         li.appendChild(deleteBtn)
         deleteBtn.textContent = 'X'
     })
 }
-
 
 filterField.addEventListener('input',function(event){
     const filteredTodos = todoListArray.filter(function(todo){
