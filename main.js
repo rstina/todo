b217ebd62cddc11e6eafb5242e730f6d3f9c024f
rstina <53047495+rstina@users.noreply.github.com>
2019-11-05 10:29:21 +0100

    Delete button removes element from list. However, it does not remove it from array yet.

--- main.js
+++ main.js
@@ -4,13 +4,10 @@ let idCounter = 0
 
 submitTodoBtn.addEventListener('click',function(event){
     event.preventDefault()
-    todoValue = document.querySelector('#todoText').value
-    console.log(todoValue)
-    console.log(todoListArray)
-    console.log(todoListArray.id)
-    todoListArray.push(todoValue)
-    todoListArray.id = 'id'+idCounter
+    todoListArray.push(document.querySelector('#todoText').value)
+    document.querySelector('#todoText').value = ''
     displayList(todoListArray)
+    idCounter++
 })
 
 const filterField = document.querySelector('#filter')
@@ -18,18 +15,32 @@ const ulList = document.querySelector('#todoList')
 // display list
 function displayList(arr){
     ulList.innerHTML = ''
-    arr.forEach( function(todo) {
+    arr.forEach( function(todo,idCounter) {
         const div = document.createElement('div')
+        div.id = 'id'+idCounter
+        ulList.appendChild(div)
         const li = document.createElement('li')
         li.textContent = todo
         div.appendChild(li)
-        ulList.appendChild(div)
+        const deleteBtn = document.createElement('button')
+        deleteBtn.id = 'ID'+idCounter
+        deleteBtn.addEventListener('click',function(event){
+            // tar bort ut listan
+            console.log(todoListArray)
+            document.getElementById(event.currentTarget.id.toLowerCase()).remove()
+            // hur ta bort ur arrayen?
+            console.log(todoListArray)
+        })
+        li.appendChild(deleteBtn)
+        deleteBtn.textContent = 'X'
     })
 }
+
+
 filterField.addEventListener('input',function(event){
     console.log(event.currentTarget.value)
     const filteredTodos = todoListArray.filter(function(todo){
         return todo.toLowerCase().includes(event.currentTarget.value.toLowerCase())
     })
-    displayList(filteredTodos)
+    displayList(filteredTodos,idCounter)
 })
