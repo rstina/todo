b778accaccec2ab83b791aa59a63ebf900dff758
rstina <53047495+rstina@users.noreply.github.com>
2019-11-06 19:14:19 +0100

    working objects

--- main.js
+++ main.js
@@ -1,35 +1,52 @@
 // creating form
 const submitTodoBtn = document.querySelector('#addTodoBtn')
 const todoField = document.querySelector('#todoText')
-const todoSelect = document.querySelector('#todoSelect')
+const todoCat = document.querySelector('#todoCat')
 const today = new Date()
+const todayISO = today.toISOString().slice(0,10)
 const todoEndDate = document.querySelector('#todoDate')
-todoEndDate.value = today.toISOString().slice(0,10)
+todoEndDate.value = todayISO
 
-// save info from form
+// save gathered information
 const todoListArr = []
 
 // filtering info
 const filterField = document.querySelector('#filter')
 const ulList = document.querySelector('#todoList')
+// radio buttons
+const radBtnAll = document.querySelector('#radAll')
+const radBtnNone = document.querySelector('#radNone')
+const radBtnFree = document.querySelector('#radFree')
+const radBtnWork = document.querySelector('#radWork')
+
+function sendRadValue(radBtn){
+    radBtn.addEventListener('click',function(event){
+        return console.log('Hello')
+    })
+}
+sendRadValue(radBtnAll)
+sendRadValue(radBtnNone)
+sendRadValue(radBtnFree)
+sendRadValue(radBtnWork)
 
 submitTodoBtn.addEventListener('click',function(event){
     event.preventDefault()
+    // create todo object and push into array
     const todoObj = {
-        text: '',
+        description: '',
         date: '',
         category: ''
     }
-    todoObj.text = todoField.value
+    todoObj.description = todoField.value
     todoObj.date = todoEndDate.value
-    todoObj.category = todoSelect.value
-    console.log(todoObj)
-    todoListArr.push(todoObj.value)
-    console.log(todoListArr)
+    todoObj.category = todoCat.value
+    todoListArr.push(todoObj)
+    // reset values
     todoField.value = ''
-    todoEndDate.value = today.toISOString().slice(0,10)
-    todoSelect.value = 'none'
+    todoEndDate.value = todayISO
+    todoCat.value = 'none'
     filterField.value = ''
+    // display list
     displayList(todoListArr)
 })
 
@@ -37,23 +54,30 @@ function displayList(arr){
     ulList.innerHTML = ''
     arr.forEach( function(todo) {
         const div = document.createElement('div')
-        const li = document.createElement('li')
+        const liText = document.createElement('li')
         const deleteBtn = document.createElement('button')
         ulList.appendChild(div)
-        div.appendChild(li)
-        li.textContent = todo
+        div.appendChild(liText)
+        // display warning if date is passed
+        const todoDate = new Date(todo.date)
+        if (todo.date !== todayISO && todoDate<today){
+            liText.textContent = `${todo.description} WARNING date passed: ${todo.date} ${todo.category} `
+        } else {
+            liText.textContent = `${todo.description} ${todo.date} ${todo.category} `
+        }
         deleteBtn.textContent = 'X'
-        li.appendChild(deleteBtn)
+        liText.appendChild(deleteBtn)
         deleteBtn.addEventListener('click',function(event){
             todoListArr.splice(todoListArr.indexOf(todo),1)
             event.currentTarget.closest('div').remove()
+            console.log(todoListArr)
         })
     })
 }
 
 filterField.addEventListener('input',function(event){
     const filteredTodos = todoListArr.filter(function(todo){
-        return todo.toLowerCase().includes(event.currentTarget.value.toLowerCase())
+        return todo.description.toLowerCase().includes(event.currentTarget.value.toLowerCase())
     })
     displayList(filteredTodos)
 })
