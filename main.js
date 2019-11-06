28682d6ec768644ca52f12f10eb4ade54fd97661
rstina <53047495+rstina@users.noreply.github.com>
2019-11-06 09:10:27 +0100

    all information in form

--- main.js
+++ main.js
@@ -1,37 +1,55 @@
+// creating form
 const submitTodoBtn = document.querySelector('#addTodoBtn')
-const todoListArray = []
 const todoField = document.querySelector('#todoText')
+const todoSelect = document.querySelector('#todoSelect')
+const today = new Date()
+const todoEndDate = document.querySelector('#todoDate')
+todoEndDate.value = today.toISOString().slice(0,10)
+
+// save info from form
+const todoListArr = []
+const todoCatsArr = []
+const todoDateArr = []
+
+// filtering info
 const filterField = document.querySelector('#filter')
 const ulList = document.querySelector('#todoList')
 
 submitTodoBtn.addEventListener('click',function(event){
     event.preventDefault()
-    todoListArray.push(todoField.value)
+    todoListArr.push(todoField.value)
+    todoDateArr.push(todoDateArr.value)
+    todoCatsArr.push(todoSelect.value)
+    console.log(todoField.value)
+    console.log(todoEndDate.value)
+    console.log(todoSelect.value)
     todoField.value = ''
+    todoEndDate.value = today.toISOString().slice(0,10)
+    todoSelect.value = 'none'
     filterField.value = ''
-    displayList(todoListArray)
+    displayList(todoListArr)
 })
 
 function displayList(arr){
     ulList.innerHTML = ''
     arr.forEach( function(todo) {
         const div = document.createElement('div')
-        ulList.appendChild(div)
         const li = document.createElement('li')
-        li.textContent = todo
-        div.appendChild(li)
         const deleteBtn = document.createElement('button')
+        ulList.appendChild(div)
+        div.appendChild(li)
+        li.textContent = todo
+        deleteBtn.textContent = 'X'
+        li.appendChild(deleteBtn)
         deleteBtn.addEventListener('click',function(event){
-            todoListArray.splice(todoListArray.indexOf(todo),1)
+            todoListArr.splice(todoListArr.indexOf(todo),1)
             event.currentTarget.closest('div').remove()
         })
-        li.appendChild(deleteBtn)
-        deleteBtn.textContent = 'X'
     })
 }
 
 filterField.addEventListener('input',function(event){
-    const filteredTodos = todoListArray.filter(function(todo){
+    const filteredTodos = todoListArr.filter(function(todo){
         return todo.toLowerCase().includes(event.currentTarget.value.toLowerCase())
     })
     displayList(filteredTodos)
